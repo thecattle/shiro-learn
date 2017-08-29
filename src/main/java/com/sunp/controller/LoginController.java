9371efe0c5f0e4b9e1cc1ceedd62b74e3309d8b3
sunpeng <sunpeng1@souche.com>
2017-08-29 22:22:11 +0800

    remembered 方法

--- src/main/java/com/sunp/controller/LoginController.java
+++ src/main/java/com/sunp/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.sunp.controller;
 
 import com.sunp.service.LoginService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,29 +12,84 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 @RequestMapping(value = "/sunp")
 public class LoginController {
 
-    private Logger logger= LoggerFactory.getLogger(LoginController.class);
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private LoginService loginService;
 
-    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    @RequestMapping(value = "/login", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
-    public String login(String username, String password){
+    public String login(String username, String password) {
         logger.info("sunp/login init");
-//        String username="sunpeng";
-//        String password="password";
-
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         token.setRememberMe(true);
-
         Subject currentUser = SecurityUtils.getSubject();
-        currentUser.login(token);
+        try {
+            currentUser.login(token);
+        } catch (UnknownAccountException uae) {
+            return "unknown account";
+        } catch (IncorrectCredentialsException ice) {
+            return "认证失败";
+        } catch (LockedAccountException lae) {
+            return "账户已锁定";
+        } catch (ExcessiveAttemptsException eae) {
+            return "登录失败次数过多";
+        } catch (AuthenticationException ae) {
+            return ae.getMessage();
+        }
+        return "login success";
+    }
 
-        return "fail";
+    @RequestMapping(value = "/loginOut", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public String loginOut() {
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            currentUser.logout();
+        } catch (UnknownAccountException uae) {
+            return "unknown account";
+        } catch (IncorrectCredentialsException ice) {
+            return "认证失败";
+        } catch (LockedAccountException lae) {
+            return "账户已锁定";
+        } catch (ExcessiveAttemptsException eae) {
+            return "登录失败次数过多";
+        } catch (AuthenticationException ae) {
+            return ae.getMessage();
+        }
+        return "logout success";
+    }
+
+    @RequestMapping(value = "/getList", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getList() {
+        Map<String, Object> map = new HashMap<>();
+        Subject currentUser = SecurityUtils.getSubject();
+
+        map.put("isRemembered", currentUser.isRemembered() ? "session过期了，我是被记住的" : currentUser.isRemembered());
+        map.put("isAuthenticated", currentUser.isAuthenticated() ? "session 还在，我是被认证的" : currentUser.isAuthenticated());
+
+
+        //登录过，可以看到自己的名字
+        //session 过期的时候，会触发当前判断，前提是登录过的用户设置了token.setRememberMe(true);
+        if (currentUser.isRemembered()){
+            map.put("name", "sunpeng");
+        }
+        //已认证，可以看到手机号
+        if (currentUser.isAuthenticated()){
+            map.put("phone", "xxxxx");
+        }
+        //未登录的情况下，只能看到其他信息
+        map.put("weather", "sunny");
+
+        return map;
     }
 }
--- src/main/java/com/sunp/shiro/myRealm.java
+++ src/main/java/com/sunp/shiro/myRealm.java
@@ -1,8 +1,8 @@
 package com.sunp.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -10,11 +10,23 @@ import org.apache.shiro.subject.PrincipalCollection;
 public class myRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
         return null;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken;
+        System.out.print("验证当前Subject时获取到token：");
+        System.out.println(ReflectionToStringBuilder.toString(token, ToStringStyle.MULTI_LINE_STYLE));
+
+        if("sunpeng".equals(token.getUsername())){
+            AuthenticationInfo authcInfo = new SimpleAuthenticationInfo("sunpeng", "password", this.getName());
+            return authcInfo;
+        }
+        if ("sunp".equals(token.getUsername())){
+            throw new AuthenticationException("用户名少了三个字母啊兄弟");
+        }
         return null;
     }
 }
