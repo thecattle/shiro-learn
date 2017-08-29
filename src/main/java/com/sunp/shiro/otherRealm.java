b644f0cd99c448a81d9e18f5a20d45f0f31c611f
sunpeng <sunpeng1@souche.com>
2017-08-29 23:25:53 +0800

    多个realm

--- src/main/java/com/sunp/shiro/otherRealm.java
+++ src/main/java/com/sunp/shiro/otherRealm.java
@@ -7,31 +7,24 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
-public class myRealm extends AuthorizingRealm {
+public class otherRealm extends AuthorizingRealm{
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-
         return null;
     }
 
-    /**
-     * 认证用户的操作
-     * @param authenticationToken
-     * @return
-     * @throws AuthenticationException
-     */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken;
-        System.out.print("验证当前Subject时获取到token：");
+        System.out.print("otherRealm 验证当前Subject时获取到token：");
         System.out.println(ReflectionToStringBuilder.toString(token, ToStringStyle.MULTI_LINE_STYLE));
 
-        if("sunpeng".equals(token.getUsername())){
-            AuthenticationInfo authcInfo = new SimpleAuthenticationInfo("sunpeng", "password", this.getName());
+        if("sunpeng1".equals(token.getUsername())){
+            AuthenticationInfo authcInfo = new SimpleAuthenticationInfo("sunpeng1", "password", this.getName());
             return authcInfo;
         }
-        if ("sunp".equals(token.getUsername())){
-            throw new AuthenticationException("用户名少了三个字母啊兄弟");
+        if ("sunp1".equals(token.getUsername())){
+            throw new AuthenticationException("用户名少了三个字母啊兄弟2");
         }
         return null;
     }
